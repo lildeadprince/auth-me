02777b4921b2e4816c6285afef2e3040b9bdf4da
Xirtid Nepla <avadend.piroserpen@gmail.com>
2021-12-21 04:02:36 +0300

    add compression

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 // import { visualizer } from 'rollup-plugin-visualizer';
 
 // @ts-ignore
@@ -10,6 +11,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression({algorithm: 'brotliCompress'})],
   // build: { rollupOptions: { plugins: [visualizer()] } },
 });
