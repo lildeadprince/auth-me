2d94ffb77665a79c60024bac0b8cd751f71d9841
Xirtid Nepla <avadend.piroserpen@gmail.com>
2021-12-23 17:38:59 +0300

    add vite server params

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,16 +7,16 @@ import compression from 'vite-plugin-compression';
 // import stylelint from 'rollup-plugin-stylelint'; // Rollup, 2yo dead, no types
 // import stylelint from '@amatlash/vite-plugin-stylelint'; // Vite, 0 config, no --fix, no link to code, explicitly "no maintenance"
 // import stylelint from 'vite-plugin-stylelint'; // Vite, no prettyprint or im dumb, but has link to code
-// import eslint from 'vite-plugin-eslint'; // it just stuck or error sometime even after dev-server restart
+// import eslint from 'vite-plugin-eslint'; // caching mechanism is broken, works only with cache: false
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: true, open: true },
+  // build: { rollupOptions: { plugins: [visualizer()] } },
+
   plugins: [
     react(),
-
     // though e.g. Firebase will do it anyway, but anyway
-    compression({algorithm: 'brotliCompress'})
+    compression({algorithm: 'brotliCompress'}) // build only
   ],
-
-  // build: { rollupOptions: { plugins: [visualizer()] } },
 });
