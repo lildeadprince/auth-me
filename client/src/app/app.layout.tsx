6c25221b1c7155bdff9cd311ca330d885db1e050
Xirtid Nepla <avadend.piroserpen@gmail.com>
2021-12-28 06:43:09 +0300

    add code splitting

--- client/src/app/app.layout.tsx
+++ client/src/app/app.layout.tsx
@@ -1,19 +1,15 @@
 import { FC, lazy, memo, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { AuthorisedRoute } from '~/routes';
+import { AuthorisedRoute, LoginForm } from '~/routes';
 import { Spinner } from '~/some-ui-kit';
 import css from './app.module.css';
 import { LogoutButton, ThemePicker } from './components';
 
-// Do not know initial app state, so lazy on both
+// Do not know initial app state, but for the app I guess it will be the Login page more often loaded first, that
+// makes sense not to split it. (Would approximately double the round trip for resources if extracted to another chunk)
 const AsyncUserInfo = lazy(() =>
   import('~/routes/user-info').then(module => ({
     default: module.UserInfo,
-  })),
-);
-const AsyncLoginForm = lazy(() =>
-  import('~/routes/login-form').then(module => ({
-    default: module.LoginForm,
   })),
 );
 
@@ -26,7 +22,7 @@ export const AppLayout: FC = memo(() => (
     <main className={css.app__page}>
       <Suspense fallback={<Spinner size={60} invert={true} />}>
         <Routes>
-          <Route path="/login" element={<AsyncLoginForm />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route
             path="/user"
             element={
--- client/src/app/components/logout-button/logout-button.tsx
+++ client/src/app/components/logout-button/logout-button.tsx
@@ -8,8 +8,12 @@ export const LogoutButton: FC = () => {
   const { setSession } = useSessionControl();
 
   const performLocalLogout = useCallback(() => setSession({ user: undefined }), []);
+
+  // start preloading next anticipated route
+  const startPreloadingLoginForm = useCallback(() => import('~/routes/login-form'), []);
+
   return (
-    <Button className={css.themePicker} onClick={performLocalLogout}>
+    <Button className={css.themePicker} onClick={performLocalLogout} onMouseOver={startPreloadingLoginForm}>
       Log Out
     </Button>
   );
